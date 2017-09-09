7d9d4927826aa7d8cabc636074f92deba29e2fe7
nikunj <me@nik4u.com>
2017-09-09 12:07:17 +0530

    Adding pcb_memcache to add permanent memcache cache bin support.

---
+++ modules/pcb_memcache/src/Cache/PermanentMemcacheBackendFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\pcb_memcache\Cache;
+
+use Drupal\memcache\MemcacheBackendFactory;
+
+/**
+ * Class PermanentMemcacheBackendFactory.
+ */
+class PermanentMemcacheBackendFactory extends MemcacheBackendFactory {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function get($bin) {
+    return new PermanentMemcacheBackend(
+      $bin,
+      $this->memcacheFactory->get($bin),
+      $this->lock,
+      $this->settings,
+      $this->checksumProvider
+    );
+  }
+
+}
